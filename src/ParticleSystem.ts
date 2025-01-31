7af9694e3bb5a2a0f8366b26948570b3a5605155
Brett Shook <madshook@gmail.com>
2025-01-31 22:30:24 +0000

    Add data debug labels and lots of other fixes

--- src/ParticleSystem.ts
+++ src/ParticleSystem.ts
@@ -17,6 +17,127 @@ export class Rect {
     isPointInside(x: number, y: number) {
         return x >= this.left && x <= this.right && y >= this.bottom && y <= this.top;
     }
+
+    getCenter() {
+        return new THREE.Vector2((this.left + this.right) / 2, (this.bottom + this.top) / 2);
+    }
+}
+
+class Gate extends Rect {
+    public isOpen: boolean = false;
+    public gateBox: THREE.LineSegments;
+    public fieldVector: THREE.Vector3 = new THREE.Vector3(0, 1, 0);
+    public fieldArrow: THREE.ArrowHelper;
+
+    constructor(rect: Rect, scene: THREE.Scene) {
+        super(rect.left, rect.right, rect.bottom, rect.top);
+
+        this.gateBox = createWireBox2D(rect, 0xffffff);
+        scene.add(this.gateBox);
+
+        const origin = new THREE.Vector3((rect.left+rect.right)/2, (rect.bottom+rect.top)/2, 0);
+        this.fieldArrow = new THREE.ArrowHelper(this.fieldVector, origin, 1, 0xffffff);
+        scene.add(this.fieldArrow);
+
+        this.close();
+    }
+
+    open() {
+        this.isOpen = true;
+        this.gateBox.material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+    }
+
+    close() {
+        this.isOpen = false;
+        this.gateBox.material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    }
+
+    update(particlePositions: Float32Array, particleCount: number) {
+        // Integrate electric field to determine if the gate is open
+        let efield = new THREE.Vector3(0, 0, 0);
+        for (let i = 0; i < particleCount; i++) {
+            let x = i * 3;
+            let y = x + 1;
+            let mx = (this.left + this.right) / 2;
+            let my = (this.bottom + this.top) / 2;
+            let r12 = new THREE.Vector3(mx - particlePositions[x], my - particlePositions[y], 0);
+            let r12n = r12.clone().normalize();
+            efield.add(r12n.divideScalar(r12.lengthSq()));
+        }
+        this.fieldArrow.setDirection(efield.clone().normalize());
+        this.fieldArrow.setLength(efield.length());
+
+        // const debugElement = document.getElementById("debug");
+        // if (debugElement) {
+        //     debugElement.innerText = efield.y.toFixed(2);
+        // }
+
+        if (efield.y < -0.4) {
+            this.open();
+            this.fieldArrow.setColor(0xff0000);
+        } 
+        else if (efield.y > 0.0) {
+            this.close();
+        }
+    }
+}
+
+class ConstantConcentration extends Rect {
+    public concentration: number;
+
+    constructor(rect: Rect, concentration: number) {
+        super(rect.left, rect.right, rect.bottom, rect.top);
+        this.concentration = concentration;
+    }
+
+    update(particlePositions: Float32Array, activeParticles: boolean[], particleCount: number, rng: MersenneTwister19937) {
+        let count = countParticles(particlePositions, this, particleCount);
+
+        if (count < this.concentration) {
+            let xDist = real(this.left, this.right);
+            let yDist = real(this.bottom, this.top);
+            for (let i = 0; i < particleCount; i++) {
+                if (!activeParticles[i]) {
+                    let x = i * 3;
+                    let y = x + 1;
+                    particlePositions[x] = xDist(rng);
+                    particlePositions[y] = yDist(rng);
+                    activeParticles[i] = true;
+                    break;
+                }
+            }
+        }
+
+        if (count > this.concentration) {
+            for (let i = 0; i < particleCount; i++) {
+                if (activeParticles[i]) {
+                    let x = i * 3;
+                    let y = x + 1;
+                    particlePositions[x] = 1000;
+                    particlePositions[y] = 1000;
+                    activeParticles[i] = false;
+                    break;
+                }
+            }
+        }
+
+        const debugElement = document.getElementById("debug2");
+        if (debugElement) {
+            debugElement.innerText = count.toString();
+        }
+    }
+}
+
+function countParticles(particlePositions: Float32Array, area: Rect, particleCount: number) {
+    let count = 0;
+    for (let i = 0; i < particleCount; i++) {
+        let x = i * 3;
+        let y = x + 1;
+        if (area.isPointInside(particlePositions[x], particlePositions[y])) {
+            count++;
+        }
+    }
+    return count;
 }
 
 export class ParticleSystem {
@@ -29,30 +150,34 @@ export class ParticleSystem {
     private geometry = new THREE.BufferGeometry();
     private blockages: Rect[] = [];
     private pumps: Rect[] = [];
+    private gates: Gate[] = [];
+    private constantConcentrations: ConstantConcentration[] = [];
 
     constructor(maxNumParticles: number, boundaryBox: Rect) {
         this.maxNumParticles = maxNumParticles;
         this.positions = new Float32Array(maxNumParticles * 3);
         this.velocities = new Float32Array(maxNumParticles * 3);
-        this.activeParticles = new Array(maxNumParticles).fill(true);
+        this.activeParticles = new Array(maxNumParticles).fill(false);
         this.boundaryBox = boundaryBox;
         this.rng = MersenneTwister19937.seed(1234);
 
-        const xPosDist = real(boundaryBox.left, boundaryBox.right);
-        const yPosDist = real(boundaryBox.bottom, boundaryBox.top);
-        const xVelDist = real(-0.1, 0.1);
-        const yVelDist = real(-0.1, 0.1);
+        // const xPosDist = real(boundaryBox.left, boundaryBox.right);
+        // const yPosDist = real(boundaryBox.bottom, boundaryBox.top);
+        // const yPosDist = real(2, boundaryBox.top);
+        // const yPosDist = real(boundaryBox.bottom, -2);
+        // const xVelDist = real(-0.1, 0.1);
+        // const yVelDist = real(-0.1, 0.1);
         for (let i = 0; i < maxNumParticles; i++) {
             let x = i * 3;
             let y = x + 1;
             let z = x + 2;
 
-            this.positions[x] = xPosDist(this.rng);
-            this.positions[y] = yPosDist(this.rng);
+            this.positions[x] = 1000;
+            this.positions[y] = 1000;
             this.positions[z] = 0;
 
-            this.velocities[x] = xVelDist(this.rng);
-            this.velocities[y] = yVelDist(this.rng);
+            this.velocities[x] = 0;
+            this.velocities[y] = 0;
             this.velocities[z] = 0;
         }
     }
@@ -69,74 +194,146 @@ export class ParticleSystem {
     }
 
     update() {
-        const brownian = real(-0.01, 0.01);
+        // Gate update
+        for (let gate of this.gates) {
+            gate.update(this.positions, this.maxNumParticles);
+        }
+
+        // Constant concentration update
+        for (let concentration of this.constantConcentrations) {
+            concentration.update(this.positions, this.activeParticles, this.maxNumParticles, this.rng);
+        }
+
+        // Particle movement
+        const brownian = real(-0.04, 0.04);
+        const Cdrag = 0.08;
+        const charge = 0.02;
         for (let i = 0; i < this.maxNumParticles; i++) {
+            if (!this.activeParticles[i]) {
+                continue;
+            }
             let x = i * 3;
             let y = x + 1;
 
-            if (this.activeParticles[i]) {
-                this.positions[x] += this.velocities[x];
-                this.positions[y] += this.velocities[y];
+            this.positions[x] += this.velocities[x];
+            this.positions[y] += this.velocities[y];
 
-                this.velocities[x] *= 0.99;
-                this.velocities[y] *= 0.99;
-
-                this.velocities[x] += brownian(this.rng);
-                this.velocities[y] += brownian(this.rng);
-
+            // Charge interaction
+            const r1 = new THREE.Vector3(this.positions[x], this.positions[y], 0);
+            let fx = 0;
+            let fy = 0;
+            for (let j = 0; j < this.maxNumParticles; j++) {
+                const forceLimit = 0.10;
                 // Charge interaction
-                const charge = -0.0005;
-                for (let j = 0; j < this.maxNumParticles; j++) {
-                    if (i !== j && this.activeParticles[j]) {
-                        let jx = j * 3;
-                        let jy = jx + 1;
-                        let dx = this.positions[jx] - this.positions[x];
-                        let dy = this.positions[jy] - this.positions[y];
-                        let r2 = dx * dx + dy * dy;
-                        let r1 = Math.sqrt(r2);
-                        // Limit the interaction range
-                        if (r1 < 0.1) {
-                            continue;
-                        }
-                        let r3 = r1*r2;
-                        this.velocities[x] += charge * dx / r3;
-                        this.velocities[y] += charge * dy / r3;
-                    }
+                if (i !== j && this.activeParticles[j]) {
+                    const jx = j * 3;
+                    const jy = jx + 1;
+                    const r2 = new THREE.Vector3(this.positions[jx], this.positions[jy], 0);
+                    const f = chargeForce(charge, r1, r2);
+                    fx += clamp(f.x, -forceLimit, forceLimit);
+                    fy += clamp(f.y, -forceLimit, forceLimit);
                 }
+            }
 
-                // Boundary collision
-                if (this.positions[x] < this.boundaryBox.left || this.positions[x] > this.boundaryBox.right) {
-                    this.velocities[x] = -this.velocities[x];
-                }
-                if (this.positions[y] < this.boundaryBox.bottom || this.positions[y] > this.boundaryBox.top) {
-                    this.velocities[y] = -this.velocities[y];
-                }
-
-                // Pump collision
-                let pumpCollision = false;
-                for (let pump of this.pumps) {
-                    if (pump.isPointInside(this.positions[x], this.positions[y])) {
-                        // Apply a force to move the particle towards the pump
-                        this.velocities[x] = 0;
-                        this.velocities[y] = 0.1;
-                        pumpCollision = true;
-                    }
-                }
-                if (pumpCollision) {
+            // Pump interaction
+            const pumpCharge = 0.2;
+            for (let pump of this.pumps) {
+                if (pump.isPointInside(this.positions[x], this.positions[y])) {
                     continue;
                 }
+                // Charge 1
+                const forceLimit = 0.5;
+                const mx = (pump.left + pump.right) / 2;
+                let r2 = new THREE.Vector3(mx, pump.top, 0);
+                let f = chargeForce(pumpCharge, r1, r2);
+                fx += clamp(f.x, -forceLimit, forceLimit);
+                fy += clamp(f.y, -forceLimit, forceLimit);
 
-                // Blockage collision
-                for (let blockage of this.blockages) {
-                    if (blockage.isPointInside(this.positions[x], this.positions[y])) {
-                        // Apply a force to move the particle away from the blockage
-                        this.velocities[y] = -0.9*this.velocities[y];
-                    }
+                // Charge 2
+                r2 = new THREE.Vector3(mx, pump.bottom, 0);
+                f = chargeForce(-pumpCharge, r1, r2);
+                fx += clamp(f.x, -forceLimit, forceLimit);
+                fy += clamp(f.y, -forceLimit, forceLimit);
+            }
+
+            this.velocities[x] += fx;
+            this.velocities[y] += fy;
+
+            // Brownian motion
+            // this.velocities[x] += brownian(this.rng);
+            // this.velocities[y] += brownian(this.rng);
+
+            // Apply drag force
+            this.velocities[x] -= Cdrag*this.velocities[x];
+            this.velocities[y] -= Cdrag*this.velocities[y];
+
+            // Boundary collision
+            let nextX = this.positions[x] + this.velocities[x];
+            let nextY = this.positions[y] + this.velocities[y];
+            if (nextX < this.boundaryBox.left || nextX > this.boundaryBox.right) {
+                this.velocities[x] = -this.velocities[x];
+            }
+            if (nextY < this.boundaryBox.bottom || nextY > this.boundaryBox.top) {
+                this.velocities[y] = -this.velocities[y];
+            }
+
+            // Pump collision
+            let pumpCollision = false;
+            for (let pump of this.pumps) {
+                if (pump.isPointInside(this.positions[x], this.positions[y])) {
+                    // Apply a force to move the particle towards the pump
+                    this.positions[y] = pump.top+1;
+                    this.velocities[x] = 0.1*this.velocities[x];
+                    this.velocities[y] = 0.1*this.velocities[y];
+                    pumpCollision = true;
                 }
+            }
+            if (pumpCollision) {
+                continue;
+            }
 
+            // Gate collision
+            let gateCollision = false;
+            for (let gate of this.gates) {
+                if (gate.isOpen) {
+                    if (gate.isPointInside(this.positions[x], this.positions[y])) {
+                        this.positions[y] = gate.bottom-1;
+                        gateCollision = true;
+                        break;
+                    }
+                } else if (gate.isPointInside(nextX, nextY)) {
+                    // Apply force to move the particle away from the gate
+                    let origin = gate.getCenter();
+                    this.velocities[x] += 0.01*(this.positions[x] - origin.x);
+                    this.velocities[y] += 0.01*(this.positions[y] - origin.y);
+                    gateCollision = true;
+                    break;
+                }
+            }
+            if (gateCollision) {
+                continue;
+            }
+
+            // Blockage collision
+            for (let blockage of this.blockages) {
+                if (blockage.isPointInside(this.positions[x], this.positions[y])) {
+                    // Apply a force to move the particle away from the blockage
+                    let dy = this.positions[y] - (blockage.bottom + blockage.top) / 2;
+                    this.velocities[y] += 0.05*dy;
+                }
             }
         }
         this.geometry.attributes.position.needsUpdate = true;
+
+        let count = countParticles(
+            this.positions, 
+            new Rect(this.boundaryBox.left, this.boundaryBox.right, 0.5, this.boundaryBox.top), 
+            this.maxNumParticles
+        );
+        const debugElement = document.getElementById("debug1");
+        if (debugElement) {
+            debugElement.innerText = count.toString();
+        }
     }
 
     getPositions() {
@@ -147,7 +344,7 @@ export class ParticleSystem {
         return this.velocities;
     }
 
-    addToScene(scene: THREE.Scene, color: number) {
+    addParticles(scene: THREE.Scene, color: number) {
         this.geometry.setAttribute('position', new THREE.BufferAttribute(this.getPositions(), 3));
         const material = new THREE.PointsMaterial({ color: color, size: 0.2 });
         const points = new THREE.Points(this.geometry, material);
@@ -175,6 +372,26 @@ export class ParticleSystem {
         scene.add(pumpBox);
         this.pumps.push(pump);
     }
+
+    addGate(gate: Rect, scene: THREE.Scene) {
+        this.gates.push(new Gate(gate, scene));
+    }
+
+    addConstantConcentration(area: Rect, concentration: number) {
+        let constantConstraint = new ConstantConcentration(area, concentration);
+        this.constantConcentrations.push(constantConstraint);
+    }
+
+}
+
+function chargeForce(charge: number, r1: THREE.Vector3, r2: THREE.Vector3) {
+    const r12 = r1.clone().sub(r2);
+    const r12n = r12.clone().normalize();
+    return r12n.multiplyScalar(charge/r12.lengthSq());
+}
+
+function clamp(value: number, min: number, max: number) {
+    return Math.min(Math.max(value, min), max);
 }
 
 function createWireBox2D(box: Rect, color: number) {
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,35 @@
 import * as THREE from 'three';
 import { ParticleSystem, Rect } from './ParticleSystem';
+import linspace from '@stdlib/array-linspace';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 30);
+camera.position.set(0, 0, 50);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const particleSystem = new ParticleSystem(100, new Rect(-20, 20, -20, 20));
-particleSystem.addToScene(scene, 0xff0000);
+const simHalfWidth = 60;
+const simHalfHeight = 20;
+const particleSystem = new ParticleSystem(1000, new Rect(-simHalfWidth, simHalfWidth, -simHalfHeight, simHalfHeight));
+particleSystem.addParticles(scene, 0xff0000);
 particleSystem.addBoundary(scene, 0xffffff);
-particleSystem.addBlockage(new Rect(-20, 20, -1, 1), scene, 0x0000ff);
-particleSystem.addPump(new Rect(-7, -3, -1.5, 1.5), scene, 0xff0000);
-particleSystem.addPump(new Rect(3, 7, -1.5, 1.5), scene, 0xff0000);
+particleSystem.addBlockage(new Rect(-simHalfWidth, simHalfWidth, -1, 1), scene, 0x0000ff);
+
+
+let locs = linspace(-0.9*simHalfWidth, 0.9*simHalfWidth, 18, {endpoint: true});
+for (let i = 0; i < locs.length; i++) {
+    const chanWidth = 1;
+    if (i % 4 == 0) {
+        particleSystem.addPump(new Rect(locs[i]-chanWidth, locs[i]+chanWidth, -1.5, 1.5), scene, 0xff0000);
+    } else {
+        particleSystem.addGate(new Rect(locs[i]-chanWidth, locs[i]+chanWidth, -1.5, 1.5), scene);
+    }
+}
+
+particleSystem.addConstantConcentration(new Rect(-simHalfWidth, simHalfWidth, -simHalfHeight, -0.5), 200);
 
 function animate() {
     // camera.position.set(0, 0, 20*Math.sin(Date.now()/1000)**2);
